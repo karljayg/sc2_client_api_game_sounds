af527d850362ea5884b1f60038d8a488a9a59a7e
KJ Garcia <kj@psistorm.com>
2023-08-02 15:46:19 -0400

    only print when there is a change of game status

--- sc2client_detect_game.py
+++ sc2client_detect_game.py
@@ -9,15 +9,12 @@ class GameState(Enum):
     STARTED = 'Undecided'
     ENDED = ['Defeat', 'Victory', 'Tie']
 
-# Add a variable to store the initial state
-initial_state = True
-
 # load configuration file
 with open('config.json') as f:
     config = json.load(f)
 
 # player to check results for
-player_name = config['player_name']
+player_names = config['player_name']
 
 def play_sound(game_event):
     pygame.mixer.init()
@@ -25,34 +22,34 @@ def play_sound(game_event):
     pygame.mixer.music.load(sound_file)
     pygame.mixer.music.play()
 
-def get_game_status():
+def get_game_status(previous_game_statuses=None):
     response = requests.get('http://localhost:6119/game')
     data = response.json()
-    print(f"Received data: {data}")  # Print the JSON data
     if not config['play_on_replay'] and data['isReplay']:
-        return None
+        return None, previous_game_statuses
     game_statuses = {}
     for player in data['players']:
-        if player['name'] in player_name:
+        if player['name'] in player_names:
             game_statuses[player['name']] = player['result']
-    return game_statuses
+    if game_statuses != previous_game_statuses:
+        print(f"Received data: {data}")  # Print the JSON data
+    return game_statuses, previous_game_statuses
 
 def monitor_game():
-    current_states = {}  # Initialize with an empty dictionary
-    is_first_run = True  # Indicates whether this is the first run of the loop
+    previous_game_statuses = {}  # Initialize with an empty dictionary
     while True:
-        game_statuses = get_game_status()
+        game_statuses, previous_game_statuses = get_game_status(previous_game_statuses)
         if game_statuses:
             for player_name, game_status in game_statuses.items():
-                current_state = current_states.get(player_name)
-                if game_status != current_state:
-                    if game_status == GameState.STARTED.value and (current_state in GameState.ENDED.value if current_state else True):
+                previous_state = previous_game_statuses.get(player_name)
+                if game_status != previous_state:
+                    if game_status == GameState.STARTED.value and (previous_state in GameState.ENDED.value if previous_state else True):
                         play_sound('start')
-                    elif game_status in GameState.ENDED.value and not is_first_run:
+                    elif game_status in GameState.ENDED.value and previous_state:
                         play_sound(game_status.lower())
-                    current_states[player_name] = game_status
-            is_first_run = False  # Update the flag after the first run
+                previous_game_statuses[player_name] = game_status
         time.sleep(1)  # Wait a second before re-checking
-          
+
 if __name__ == "__main__":
     monitor_game()
+
